ce6f0539cf4407c372c320d4b658fa6053d67c68
Dogam <lochi.ranatunga@gmail.com>
2018-07-19 22:40:22 +0530

    >created new method in player services to sort everything

---
+++ src/main/java/com/example/demo/services/PlayerExtended.java
@@ -0,0 +1,51 @@
+package com.example.demo.services;
+
+public class PlayerExtended {
+
+	public String name;
+	public Double KD;
+	public Double totalEliminations;
+	public Double totalDeaths;
+	public Double totalDamage;
+	public Double totalHealing;
+	public Double totalUltimates;
+	public Double totalFinalBlows;
+	public Double totalTimePlayed;
+	
+	public int playerId;
+	public int teamId;
+	public String role;
+	public String team;
+	public Double eliminations;
+	public Double deaths;
+	public Double damage;
+	public Double healing;
+	public Double ultimates;
+	public Double finalBlows;
+	public Double timePlayed;
+	
+	public PlayerExtended (String name, Double KD, Double totalEliminations, Double totalDeaths, Double totalDamage, Double totalHealing, Double totalUltimates, Double totalFinalBlows, int playerId,int teamId,String role,String team,Double eliminations,Double deaths,Double damage,Double healing,Double ultimates,Double finalBlows,Double timePlayed) {
+		
+		this.name = name;
+		this.KD =KD;
+		this.totalDamage =totalDamage;
+		this.totalDeaths = totalDeaths;
+		this.totalEliminations = totalEliminations;
+		this.totalFinalBlows = totalFinalBlows;
+		this.totalHealing =  totalHealing;
+		this.totalTimePlayed = totalTimePlayed;
+		this.totalUltimates = totalUltimates;
+		this.playerId = playerId;
+		this.role = role;
+		this.team = team;
+		this.eliminations = eliminations;
+		this.deaths = deaths;
+		this.damage = damage;
+		this.healing = healing;
+		this.ultimates = ultimates;
+		this.finalBlows =finalBlows;
+		this.timePlayed = timePlayed;
+		
+		
+	}
+}
--- src/main/java/com/example/demo/services/PlayerService.java
+++ src/main/java/com/example/demo/services/PlayerService.java
@@ -42,5 +42,97 @@ public class PlayerService {
 		return playerKD;
 		
 	}
+	
+	public Iterable<PlayerExtended> findByCriteria (String criteria) {
+		
+		List<PlayerExtended> playerExtended = new ArrayList<PlayerExtended>();
+		
+		List<Player> players = repo.findAll();
+		
+		for (Player p : players) {
+			
+			playerExtended.add(new PlayerExtended(p.name,p.eliminations/p.deaths, p.timePlayed/10*p.eliminations, p.timePlayed/10*p.deaths, p.timePlayed/10*p.damage, p.timePlayed/10*p.healing, p.timePlayed/10*p.ultimates, p.timePlayed/10*p.finalBlows, p.playerId, p.teamId, p.role, p.team, p.eliminations, p.deaths, p.damage, p.healing, p.ultimates, p.finalBlows, p.timePlayed));
+			
+		}
+		
+		switch(criteria) {
+		
+			case "kd": Collections.sort(playerExtended, new Comparator<PlayerExtended>() {
+				
+				@Override
+			    public int compare(PlayerExtended o1, PlayerExtended o2) {
+			        return (Double.compare(o2.KD, o1.KD));
+			    }
+							
+			});
+			break;
+				
+			case "totalEliminations":Collections.sort(playerExtended, new Comparator<PlayerExtended>() {
+				
+				@Override
+			    public int compare(PlayerExtended o1, PlayerExtended o2) {
+			        return (Double.compare(o2.totalEliminations, o1.totalEliminations));
+			    }
+							
+			});
+			break;
+		
+			case "totalDeaths":Collections.sort(playerExtended, new Comparator<PlayerExtended>() {
+				
+				@Override
+			    public int compare(PlayerExtended o1, PlayerExtended o2) {
+			        return (Double.compare(o2.totalDeaths, o1.totalDeaths));
+			    }
+							
+			});
+			break;
+				
+			case "totalDamage":Collections.sort(playerExtended, new Comparator<PlayerExtended>() {
+				
+				@Override
+			    public int compare(PlayerExtended o1, PlayerExtended o2) {
+			        return (Double.compare(o2.totalDamage, o1.totalDamage));
+			    }
+							
+			});
+			break;
+				
+			case "totalHealing":Collections.sort(playerExtended, new Comparator<PlayerExtended>() {
+				
+				@Override
+			    public int compare(PlayerExtended o1, PlayerExtended o2) {
+			        return (Double.compare(o2.totalHealing, o1.totalHealing));
+			    }
+							
+			});
+			break;
+				
+			case "totalUltimates":Collections.sort(playerExtended, new Comparator<PlayerExtended>() {
+				
+				@Override
+			    public int compare(PlayerExtended o1, PlayerExtended o2) {
+			        return (Double.compare(o2.totalUltimates, o1.totalUltimates));
+			    }
+							
+			});
+			break;
+				
+			case "totalFinalBlows":Collections.sort(playerExtended, new Comparator<PlayerExtended>() {
+				
+				@Override
+			    public int compare(PlayerExtended o1, PlayerExtended o2) {
+			        return (Double.compare(o2.totalFinalBlows, o1.totalFinalBlows));
+			    }
+							
+			});
+			break;
+				
+			default : break;
+
+		}
+		
+		return playerExtended;
+		
+	}
 
 }
